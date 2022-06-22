005d67eb0a82f18c0b045067f630efa85b159698
danielinaciof <daniel.scp10@gmail.com>
2022-06-22 14:26:43 +0100

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,10 @@
 
-import { Link } from 'react-router-dom';
 
 import './styles.scss';
 export function Header() {
     return (
         <header>
-            <a href='/'><img src= "/images/logo.png"/></a>
+            <a href='/'><img src= "/images/logo.png" alt="logo" /></a>
            
 
         </header>
